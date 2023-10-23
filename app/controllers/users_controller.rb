c06f3f476f8519bd19c837decabe2d99da8c7fa5
Takumi-Tajima <116613691+Takumi-Tajima@users.noreply.github.com>
2023-10-23 06:25:57 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    # @user = User.first
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #Success
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      # redirect_to user_path(@user)
+    else
+      #Failuer
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+    # Strong Parameters
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
